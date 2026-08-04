d91cf546b3397263013bf94aa2823718cbd0f4ba
Mkj46 <madhav46krishna@gmail.com>
2026-08-04 10:51:59 +0530

    Configure PostgreSQL and SQLAlchemy database connection

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
 from app.api.student import router as student_router
+from app.db.database import engine
+from sqlalchemy import text
 
 app = FastAPI(
     title="EduSync AI API",
     version="1.0.0"
 )
+
+@app.on_event("startup")
+def test_database_connection():
+    try:
+        with engine.connect() as connection:
+            connection.execute(text("SELECT 1"))
+        print("✅ Database connected successfully!")
+    except Exception as e:
+        print("❌ Database connection failed!")
+        print(e)
 
 app.include_router(student_router, prefix="/students", tags=["Students"])
 
